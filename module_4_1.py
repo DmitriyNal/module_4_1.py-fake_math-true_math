e36c88df8f2d7c8f8157647a1783a6f77acf72ff
DmitriyNal <dima7kiper@yandex.ru>
2024-08-17 12:37:23 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,17 @@
+from true_math import divide as div
+from fake_math import divide as dv
+
+
+result1 = div(69, 3)
+result2 = div(3, 0)
+
+
+result3 = dv(49, 7)
+result4 = dv(15, 0)
+
+
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
